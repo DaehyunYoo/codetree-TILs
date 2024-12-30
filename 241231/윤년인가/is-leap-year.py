aae0fefc8687b062aadb4ee4ca5780affcd940ea
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2024-12-31 00:00:29 +0900

    Title: 윤년인가, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/윤년인가/is-leap-year.py
+++ 241231/윤년인가/is-leap-year.py
@@ -1,9 +1,14 @@
-y = int(input())
+# 연도 입력 받기
+year = int(input())
 
-if y % 4 == 0:
-    print('true')
-elif y % 100 == 0 
-    if y % 400 != 0:
-        print('true')
-else:
-    print('false')
+# 윤년 판단 로직
+is_leap_year = False
+
+if year % 4 == 0:  # 4로 나누어 떨어지면 일단 윤년
+    is_leap_year = True
+    if year % 100 == 0:  # 100으로 나누어 떨어지면
+        if year % 400 != 0:  # 400으로 나누어 떨어지지 않으면 평년
+            is_leap_year = False
+
+# 결과 출력
+print(str(is_leap_year).lower())
