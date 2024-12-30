395ad374bf5bba305f52a2942e643f1d13a1f0df
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2024-12-30 23:47:50 +0900

    Title: 장학금, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241230/장학금/scholarship.py
+++ 241230/장학금/scholarship.py
@@ -1,8 +1,16 @@
-a, b = map(int, input().split())
+# 중간고사와 기말고사 점수 입력받기
+mid_score, final_score = map(int, input().split())
 
-if a >= 90 and b >= 95:
-    print(10)
-elif a >= 90 and 90 <= b < 95:
-    print(5)
-else:
-    print(0)
+# 장학금 계산하기
+scholarship = 0
+
+# 중간고사가 90점 이상인 경우에만 기말고사 점수에 따라 장학금 지급
+if mid_score >= 90:
+    if final_score >= 95:
+        scholarship = 100000  # 95점 이상이면 10만원
+    elif final_score >= 90:
+        scholarship = 50000   # 90점 이상이면 5만원
+    # 그 외의 경우는 장학금 0원 (이미 0으로 초기화되어 있음)
+
+# 장학금 출력
+print(scholarship)
