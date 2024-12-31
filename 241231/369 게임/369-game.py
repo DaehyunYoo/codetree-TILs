1311bcc5b44bf60df2f1b475f96ec44c856baccc
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2024-12-31 15:41:45 +0900

    Title: 369 게임, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241231/369 게임/369-game.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if '3' in str(i).split() or '6' in str(i).split() or '9' in str(i).split():
+        print(0, end=" ")
+    else:
+        print(i, end=" ")
