a9b8931715f46b5c9488d248c880f688c8ba1a86
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2024-12-31 15:44:17 +0900

    Title: 369 게임, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241231/369 게임/369-game.py
+++ 241231/369 게임/369-game.py
@@ -3,5 +3,7 @@ n = int(input())
 for i in range(1, n+1):
     if '3' in list(str(i).split()) or '6' in list(str(i).split()) or '9' in list(str(i).split()):
         print(0, end=" ")
+    elif int(i) % 3 == 0:
+        print(0, end=" ")
     else:
         print(i, end=" ")
