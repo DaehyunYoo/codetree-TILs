a1d15931860d9ef60ffb2be5ed465e351585cee6
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2024-12-31 15:47:37 +0900

    Title: 369 게임, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/369 게임/369-game.py
+++ 241231/369 게임/369-game.py
@@ -1,9 +1,16 @@
+# 입력 받기
 n = int(input())
 
-for i in range(1, n+1):
-    if '3' in list(str(i).split()) or '6' in list(str(i).split()) or '9' in list(str(i).split()):
-        print(0, end=" ")
-    elif int(i) % 3 == 0:
-        print(0, end=" ")
-    else:
-        print(i, end=" ")
+# 1부터 n까지 반복
+for i in range(1, n + 1):
+   # 현재 숫자가 3의 배수인지 확인
+   if i % 3 == 0:
+       print(0, end=' ')
+       continue
+       
+   # 현재 숫자에 3,6,9가 포함되어 있는지 확인
+   num_str = str(i)
+   if '3' in num_str or '6' in num_str or '9' in num_str:
+       print(0, end=' ')
+   else:
+       print(i, end=' ')
