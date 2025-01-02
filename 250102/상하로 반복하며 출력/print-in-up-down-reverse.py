f695bcf4abf440183b45363d65d55600a036b926
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-02 12:51:43 +0900

    Title: 상하로 반복하며 출력, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ 250102/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -1,11 +1,11 @@
+# 변수 선언 및 입력
 n = int(input())
-
-for i in range(1, n + 1):
-    pattern = ''
-    for j in range(1, n + 1):
-        if j % 2 == 1:
-            pattern += str(i)
-        else:
-            opposite = n - i + 1
-            pattern += str(opposite)
-    print(pattern)
+	
+# 상하로 반복하여 출력합니다.
+for i in range(n):
+	for j in range(n):
+		if j % 2 == 0:
+			print(i + 1, end="")
+		else:
+			print(n - i, end="")
+	print()
