7cb41616a0669d1514c577acdd79d6d03a96b6d9
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-02 10:19:01 +0900

    Title: 별표 출력하기 13, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/별표 출력하기 13/print-start-13.py
+++ 250102/별표 출력하기 13/print-start-13.py
@@ -3,9 +3,9 @@ n = int(input())
 print('* ' * n)
 
 for i in range(1, n):
-    print('* '* i)
-
-for i in range(n-1, 0, -1):
+    
     print('* ' * i)
 
-print('* '* n)
+    print('* ' * (n - i))
+
+print('* ' * n)
