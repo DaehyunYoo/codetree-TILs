f8bf435a3ed278c970797170b6813969f8ba91c1
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-03 15:47:49 +0900

    Title: 알파벳 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250103/알파벳 출력하기/print-alphabet.py
+++ 250103/알파벳 출력하기/print-alphabet.py
@@ -4,8 +4,8 @@ num = 65
 for i in range(n):
     for j in range(i+1):
         print(chr(num), end="")
-        if num != 91:
-            num+= 1
+        if num != 90:
+            num += 1
         else:
             num = 65
     print()
