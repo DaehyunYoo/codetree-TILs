440daf2cb9eb8eb6a341cfc14c7f50857eea6e4e
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-04 17:14:54 +0900

    Title: 100 도달하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250104/100 도달하기/reach-100.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+a, b = 1, n
+print(a, b, end=" ")
+
+for i in range(3, 11):
+    a, b = b, a+b
+    if b > 100:
+        print(b, end=" ")
+        break
+    else:
+        print(b, end=" ")
