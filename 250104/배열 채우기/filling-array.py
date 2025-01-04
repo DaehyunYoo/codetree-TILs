1df3f476efab6635bf88f91803c9ac990002de2e
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-04 16:06:48 +0900

    Title: 배열 채우기, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250104/배열 채우기/filling-array.py
+++ 250104/배열 채우기/filling-array.py
@@ -6,6 +6,8 @@ while True:
     
     for num in input_numbers:
         if num == 0:  # 0을 입력받으면
-            print(*numbers[::-1])  # 지금까지 입력받은 숫자들 출력
-            exit()  # 프로그램 종료
+            print(*numbers[::-1])  # 지금까지 입력받은 숫자들 역순으로 출력
+            break  # while 루프 종료
         numbers.append(num)  # 0이 아닌 숫자는 리스트에 추가
+    if 0 in input_numbers:  # 0이 입력되었다면
+        break  # while 루프 종료
