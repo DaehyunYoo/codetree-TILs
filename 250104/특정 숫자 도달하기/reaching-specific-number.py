ddd58603753e4e8c1ee292e44dfca62bb8541a91
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-04 15:44:27 +0900

    Title: 특정 숫자 도달하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250104/특정 숫자 도달하기/reaching-specific-number.py
+++ 250104/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,14 +1,18 @@
-lst = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
 result = []
-
-for i in lst:
-    if i >= 250:
-        break
-    else:
-        result.append(i)
-
-sumation = sum(result)
-mean = sum(result) / len(result)
-
-print(sumation, mean)
     
+# 2500 미만의 숫자들만 결과 리스트에 추가
+for num in numbers:
+    if num >= 250:
+        break
+    result.append(num)
+
+# 결과가 없는 경우 처리
+if not result:
+    print(0, 0)
+
+# 합계와 평균 계산
+total = sum(result)
+average = total / len(result)
+
+print(total, average)
