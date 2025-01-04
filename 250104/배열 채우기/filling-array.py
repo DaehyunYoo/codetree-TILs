45c17a70ef3e59bf136ce8f8c37e7b9d41d429ec
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-04 16:10:14 +0900

    Title: 배열 채우기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250104/배열 채우기/filling-array.py
+++ 250104/배열 채우기/filling-array.py
@@ -1,13 +1,9 @@
-numbers = []  # 입력받은 숫자들을 저장할 리스트
+arr = list(map(int, input().split()))
 
-while True:
-    # 공백을 기준으로 숫자들을 입력받음
-    input_numbers = list(map(int, input().split()))
-    
-    for num in input_numbers:
-        if num == 0:  # 0을 입력받으면
-            print(*numbers[::-1])  # 지금까지 입력받은 숫자들 역순으로 출력
-            break  # while 루프 종료
-        numbers.append(num)  # 0이 아닌 숫자는 리스트에 추가
-    if 0 in input_numbers:  # 0이 입력되었다면
-        break  # while 루프 종료
+result = []
+for elem in arr:
+    if elem == 0:
+        break
+    result.append(elem)
+
+print(*result[::-1])
