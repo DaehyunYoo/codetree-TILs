e8c34914c33705e208f4a670edd65b100258c3b2
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-04 17:09:50 +0900

    Title: 숫자들의 배수, Time: 38ms, Memory: 16MB, Status: Passed - CodeTree

--- 250104/숫자들의 배수/multiple-of-numbers.py
+++ 250104/숫자들의 배수/multiple-of-numbers.py
@@ -2,7 +2,7 @@ n = int(input())
 
 print(n, end=" ")
 for i in range(2, 11):
-    if (n * i) % 5 == 0 and (n * i-1) % 5 == 0:
+    if (n * i) % 5 == 0 and (n * (i-1)) % 5 == 0:
         print(n * i, end=" ")
         break
     print(n * i, end=" ")
