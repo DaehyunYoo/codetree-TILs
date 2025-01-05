7c0afeee53510ea3330a2866648e9ff5775e7b73
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-05 13:22:14 +0900

    Title: 십의 자리 숫자, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250105/십의 자리 숫자/number-of-tens-digit.py
+++ 250105/십의 자리 숫자/number-of-tens-digit.py
@@ -1,12 +1,15 @@
-nums = list(input().split())
+nums = input().split()
+cnt = [0] * 10  # 0부터 9까지의 카운트를 저장할 리스트
 
-cnt = [0 for _ in range(10)]
+for num in nums:
+    # 문자열을 정수로 변환
+    n = int(num)
+    # 십의 자리가 있다면 (10 이상이면)
+    if n >= 10:
+        cnt[n // 10] += 1  # 십의 자리 수를 카운트
+    # elif n > 0:  # 0보다 큰 한 자리 수라면
+    #     cnt[n] += 1  # 해당 숫자를 카운트
 
-for i in nums:
-    if len(i) == 2:
-        # print(int(i[0]))
-        cnt[int(i[0]) - 1] += 1
-
+# 결과 출력
 for i in range(1, 10):
-    print(f'{i} - {cnt[i-1]}')
-
+    print(f'{i} - {cnt[i]}')
