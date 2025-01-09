561a18d1c9f86139c96a45f69a373013adf7296b
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-09 11:36:15 +0900

    Title: 문자열 길이의 합, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250109/문자열 길이의 합/sum-length-of-string.py
+++ 250109/문자열 길이의 합/sum-length-of-string.py
@@ -2,7 +2,8 @@ n = int(input())
 cnt, cnt_a = 0, 0
 for i in range(n):
     word = input()
-    cnt_a += word.count('a')
     cnt += len(word)
+    if word[0] == 'a':  # 첫 번째 문자가 'a'인 경우만 카운트
+        cnt_a += 1
 
 print(cnt, cnt_a)
