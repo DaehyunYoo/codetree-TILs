dc26dbcfb3ee99d2b09c3bb0806edcc3b144ecc4
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-10 12:29:00 +0900

    Title: 두 번째를 첫 번째로, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250110/두 번째를 첫 번째로/second-to-first.py
@@ -0,0 +1,9 @@
+given_str = input()
+result = ''
+for i in range(len(given_str)):
+    if given_str[i] == given_str[1]:
+        result += given_str[0]
+    else:
+        result += given_str[i]
+
+print(result)
