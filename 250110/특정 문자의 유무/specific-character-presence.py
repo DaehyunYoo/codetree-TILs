a19346297539e63cfd2a2ec52a8666b5562f1744
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-10 10:43:57 +0900

    Title: 특정 문자의 유무, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250110/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,12 @@
+given_str = input()
+
+if 'ee' in given_str:
+    print('Yes', end=" ")
+else:
+    print('No', end=" ")
+
+if 'ab' in given_str:
+    print('Yes', end=" ")
+else:
+    print('No', end=" ")
+
