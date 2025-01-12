5ce57484ce373c4e89bd9a222840fd2b5b398525
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-12 17:47:37 +0900

    Title: 미는 횟수, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250112/미는 횟수/number-of-pushes.py
+++ 250112/미는 횟수/number-of-pushes.py
@@ -4,13 +4,15 @@ cnt = 0
 found = False
 
 for i in range(len(A)):
-    temp = A[-i:] + A[:-i]
+    # 한 칸씩 오른쪽으로 밀기
+    temp = A[-1] + A[:-1]  # 항상 마지막 문자 하나만 앞으로 가져옴
     cnt += 1
+    
     if temp == B:
         found = True
         print(cnt)
         break
-    A = temp
+    A = temp  # 다음 반복을 위해 temp를 A에 저장
 
 if not found:
     print(-1)
