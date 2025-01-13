6e56a319ce213f2835a3bc2ae25863b0b3671eef
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-13 12:07:40 +0900

    Title: 최소공배수 구하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250113/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250113/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,10 +1,9 @@
 n, m = map(int, input().split())
 
-def lcm(n, m):
-    multiple = x
-    while True:
-        if multiple % y == 0:
-            return multiple
-        multiple += x
+def lcm(x, y):
+    # 큰 수부터 시작해서 두 수의 곱까지 확인
+    for i in range(max(x, y), (x * y) + 1):
+        if i % x == 0 and i % y == 0:
+            return i
 
 print(lcm(n, m))
