4870628e03a19415c1be1d9db3906e85ac85e3f5
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-13 11:56:48 +0900

    Title: 최대공약수 구하기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250113/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250113/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,11 +1,8 @@
 n, m = map(int, input().split())
 
-def csc(x, y):
-    result = 0
-    for i in range(1, min(x, y)):
-        if x % i == 0 and y % i == 0:
-            result = i
-    
-    print(result)
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
 
-csc(n, m)
+print(gcd(n, m))
