50d8f0a6cc5ab810f7dde8577a005c0b6589a3b2
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-13 12:07:00 +0900

    Title: 최소공배수 구하기, Time: 75ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 250113/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+def lcm(n, m):
+    multiple = x
+    while True:
+        if multiple % y == 0:
+            return multiple
+        multiple += x
+
+print(lcm(n, m))
