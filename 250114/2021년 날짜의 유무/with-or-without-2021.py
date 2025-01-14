27a5c25b7a1508a7d2fae2b1694f0399c40f99fb
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-14 12:32:29 +0900

    Title: 2021년 날짜의 유무, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/2021년 날짜의 유무/with-or-without-2021.py
+++ 250114/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,14 +1,22 @@
 M, D = map(int, input().split())
 
 def is_month(x, y):
+    # 월이 1~12 범위를 벗어나면 False
     if not 1 <= x <= 12:
         return False
     
-    if x == 2 and not 1 <= y <= 28:
-        return False
-    
-    if y < 1 or y > 31:
-        return False
+    # 2월은 28일까지
+    if x == 2:
+        if not 1 <= y <= 28:
+            return False
+    # 4, 6, 9, 11월은 30일까지
+    elif x in [4, 6, 9, 11]:
+        if not 1 <= y <= 30:
+            return False
+    # 나머지 월은 31일까지
+    else:
+        if not 1 <= y <= 31:
+            return False
 
     return True
 
