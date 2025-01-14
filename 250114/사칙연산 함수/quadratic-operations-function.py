517f5b53a6bf284029760cc77d1e556e2af5578c
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-14 11:42:13 +0900

    Title: 사칙연산 함수, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/사칙연산 함수/quadratic-operations-function.py
+++ 250114/사칙연산 함수/quadratic-operations-function.py
@@ -1,26 +1,30 @@
 a, o, c = input().split()
 
 def plus(x, y):
-    return x + y
+    return int(x) + int(y)
 
 def minus(x, y):
-    return x - y
+    return int(x) - int(y)
 
 def divide(x, y):
-    return x // y
+    if int(y) == 0:  # 0으로 나누는 경우 처리
+        return "나눗셈 불가"
+    return int(x) // int(y)
 
 def multiple(x, y):
-    return int(x)*int(y)
-
+    return int(x) * int(y)
 
 if o not in ['+', '-', '/', '*']:
     print('False')
 else:
-    if o == '+':
-        print(f'{a} {o} {c} = {plus(a, c)}')
-    elif o == '-':
-        print(f'{a} {o} {c} = {minus(a, c)}')
-    elif o == '/':
-        print(f'{a} {o} {c} = {divide(a, c)}')
-    else:
-        print(f'{a} {o} {c} = {multiple(a, c)}')
+    try:
+        if o == '+':
+            print(f'{a} {o} {c} = {plus(a, c)}')
+        elif o == '-':
+            print(f'{a} {o} {c} = {minus(a, c)}')
+        elif o == '/':
+            print(f'{a} {o} {c} = {divide(a, c)}')
+        else:
+            print(f'{a} {o} {c} = {multiple(a, c)}')
+    except ValueError:  # 숫자가 아닌 입력을 처리
+        print('숫자를 입력해주세요')
