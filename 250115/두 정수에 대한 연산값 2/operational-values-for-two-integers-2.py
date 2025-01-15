2353088dcbf103e67f4051a7851752902b29247d
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-15 11:33:38 +0900

    Title: 두 정수에 대한 연산값 2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250115/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 250115/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -4,6 +4,13 @@ def abc(x, y):
     small = min(x, y) + 10
     large = max(x, y) * 2
 
-    print(small, large)
+    if x > y:
+        x = large
+        y = small
+    else:
+        x = small
+        y = large
+
+    print(x, y)
 
 abc(a, b)
