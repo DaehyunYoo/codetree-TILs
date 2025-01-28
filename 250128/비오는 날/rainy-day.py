444ac50e10b8abcea47a160403b8181a7075b703
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-28 13:40:40 +0900

    Title: 비오는 날, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250128/비오는 날/rainy-day.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+class result:
+    def __init__(self, date, day, weather):
+        self.date = date
+        self.day = day
+        self.weather = weather
+
+cnt = []
+for _ in range(n):
+    date, day, weather = input().split()
+    cnt.append(result(date, day, weather))
+
+
+for i in range(n):
+    if cnt[i].weather == 'Rain':
+        print(cnt[i].date, cnt[i].day, cnt[i].weather)
+        break
