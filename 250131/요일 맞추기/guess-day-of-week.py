2c3edbb03737addd882952fc727fbc2e11624f2f
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-01-31 11:25:16 +0900

    Title: 요일 맞추기, Time: 79ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 250131/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,38 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+days = 0
+
+reverse = False
+
+while True:
+    if m1 == m2 and d1 == d2:
+        break
+
+    if (m1 == m2 and d1 > d2) or (m1 > m2):
+        resvese = True
+        if d1 == 1:
+            days += 1
+            d1 = month_days[m1-1]
+            m1 -= 1
+        d1 -= 1
+        days += 1
+    
+    else:
+        if d1 == month_days[m1]:
+            d1 = 1
+            days += 1
+            m1 += 1
+        
+        d1 += 1
+        days += 1
+
+result = days % 7
+
+if reverse:
+    print(day_of_week[result])
+else:
+    print(day_of_week[-result + 7])
