45abbce5db27e6131b84abe5975e0365f7391abd
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-11 14:48:54 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 138ms, Memory: 48MB, Status: Passed - CodeTree

--- 250211/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250211/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,36 +1,49 @@
-a_x1, a_y1, a_x2, a_y2 = map(int, input().split())
-b_x1, b_y1, b_x2, b_y2 = map(int, input().split())
+OFFSET = 1000
+MAX_R = 2000
 
-# 두 번째 직사각형이 첫 번째 직사각형을 완전히 포함하는 경우
-if b_x1 <= a_x1 and b_x2 >= a_x2 and b_y1 <= a_y1 and b_y2 >= a_y2:
-    result = 0
+# 변수 선언 및 입력
+n = 2
+rects = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-# 겹치는 구간이 없을 때
-elif b_y2 < a_y1 or b_x2 < a_x1 or b_x1 > a_x2 or b_y1 > a_y2:
-    result = abs(a_x2 - a_x1) * abs(a_y2 - a_y1)
+checked = [
+    [0] * (MAX_R + 1)
+    for _ in range(MAX_R + 1)
+]
 
+for i, (x1, y1, x2, y2) in enumerate(rects, start=1):
+    # OFFSET을 더해줍니다.
+    x1, y1 = x1 + OFFSET, y1 + OFFSET
+    x2, y2 = x2 + OFFSET, y2 + OFFSET
+    
+    # 직사각형에 주어진 순으로 1, 2 번호를 붙여줍니다.
+    # 격자 단위로 진행하는 문제이므로
+    # x2, y2에 등호가 들어가지 않음에 유의합니다.
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            checked[x][y] = i
+
+# 1, 2 순으로 붙였는데도
+# 아직 숫자 1로 남아있는 곳들 중 최대 최소 x, y 값을 전부 계산합니다.
+min_x, max_x, min_y, max_y = MAX_R, 0, MAX_R, 0
+first_rect_exist = False
+for x in range(MAX_R + 1):
+    for y in range(MAX_R + 1):
+        if checked[x][y] == 1:
+            first_rect_exist = True
+            min_x = min(min_x, x)
+            max_x = max(max_x, x)
+            min_y = min(min_y, y)
+            max_y = max(max_y, y)
+
+# 넓이를 계산합니다.
+# Case 1. 첫 번째 직사각형이 전혀 남아있지 않다면 넓이는 0입니다.
+if not first_rect_exist:
+    area = 0
+# Case 2. 첫 번째 직사각형이 남아있다면, 넓이를 계산합니다.
 else:
-    # 겹치는 구간이 있을 때
-    original_width = abs(a_x2 - a_x1)
-    original_height = abs(a_y2 - a_y1)
-    min_area = float('inf')
-    
-    # 왼쪽으로 완전히 밀 경우
-    area = original_height * original_width
-    min_area = min(min_area, area)
-    
-    # 오른쪽으로 완전히 밀 경우
-    area = original_height * original_width
-    min_area = min(min_area, area)
-    
-    # 위로 완전히 밀 경우
-    area = original_height * original_width
-    min_area = min(min_area, area)
-    
-    # 아래로 완전히 밀 경우
-    area = original_height * original_width
-    min_area = min(min_area, area)
-    
-    result = min_area
+    area = (max_x - min_x + 1) * (max_y - min_y + 1)
 
-print(result)
+print(area)
