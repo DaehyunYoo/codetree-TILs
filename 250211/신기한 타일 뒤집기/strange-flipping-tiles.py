3d34c875ef2fdc3a482a326f0284fcba72d6d4c3
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-11 12:41:00 +0900

    Title: 신기한 타일 뒤집기, Time: 88ms, Memory: 21MB, Status: Passed - CodeTree

--- 250211/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 250211/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,25 +1,24 @@
 N = int(input())
-line = [0] * 200000  # 배열 크기를 크게 증가
-point = 100000       # 중앙점도 그에 맞게 조정
+line = [0] * 200000
+point = 100000
 
 for _ in range(N):
     x, c = input().split()
     x = int(x)
     
     if c == 'L':
-        # 현재 위치에서 왼쪽으로 x만큼 칠하기
-        for i in range(point, point - x, -1):
-            line[i] = -1
-        point = point - x  # 마지막 위치로 이동
+        # 현재 위치부터 왼쪽으로 x칸 이동하며 칠하기
+        for i in range(x):
+            line[point - i] = -1
+        point = point - x + 1  # 마지막 위치로 이동
     else:
-        # 현재 위치에서 오른쪽으로 x만큼 칠하기
-        for i in range(point, point + x):
-            line[i] = 1
+        # 현재 위치부터 오른쪽으로 x칸 이동하며 칠하기
+        for i in range(x):
+            line[point + i] = 1
         point = point + x - 1  # 마지막 위치로 이동
 
 # 흰색(-1)과 검은색(1) 타일 개수 계산
-white = 0
-black = 0
+white = black = 0
 for tile in line:
     if tile == -1:
         white += 1
