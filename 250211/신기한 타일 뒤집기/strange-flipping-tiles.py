afaa26eff9c2541be6e3cc0dfdc4f1e171ae4ecf
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-11 12:33:40 +0900

    Title: 신기한 타일 뒤집기, Time: 77ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 250211/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 250211/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -2,10 +2,12 @@ def solve_tile_problem():
     # 명령의 개수 N 입력
     N = int(input())
     
-    # 타일 상태를 저장할 배열 (-1: 회색, 1: 검은색, 0: 초기상태)
-    # 충분한 크기의 배열 생성 (왼쪽, 오른쪽 이동 고려)
-    tiles = [0] * 2001
-    position = 1000  # 중앙에서 시작
+    # 최대 이동 가능 거리를 고려한 배열 크기 설정
+    # N(최대 1000) * x(최대 100) = 100,000
+    # 양방향 이동을 고려하여 중앙에서 ±100,000
+    # 여유를 두어 300,000 크기로 설정
+    tiles = [0] * 300000
+    position = 150000  # 중앙에서 시작
     
     # N개의 명령 처리
     for _ in range(N):
@@ -13,12 +15,12 @@ def solve_tile_problem():
         x = int(x)
         
         if direction == 'L':
-            # 왼쪽으로 이동하면서 회색으로 칠하기
+            # 왼쪽으로 이동하면서 회색(-1)으로 칠하기
             for i in range(x):
                 position -= 1
                 tiles[position] = -1
         else:  # direction == 'R'
-            # 오른쪽으로 이동하면서 검은색으로 칠하기
+            # 오른쪽으로 이동하면서 검은색(1)으로 칠하기
             for i in range(x):
                 tiles[position] = 1
                 position += 1
