c081f84b848e57d7244734dafc5ac1c5558d88ca
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-12 14:36:26 +0900

    Title: 좌우로 움직이는 로봇, Time: 258ms, Memory: 56MB, Status: Passed - CodeTree

--- 250212/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 250212/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -25,10 +25,19 @@ for _ in range(M):
             current_position += 1
         position_b.append(current_position)
 
+# print(position_a)
+# print(position_b)
+
 cnt = 0
-time = min(len(position_a), len(position_b))
-for i in range(time):
+time = max(len(position_a), len(position_b))
+# 이동이 멈추면 마지막 위치 고정
+
+# 마지막 위치로 리스트 확장
+position_a += [position_a[-1]] * (time - len(position_a))
+position_b += [position_b[-1]] * (time - len(position_b))
+
+for i in range(1, time):
     if position_a[i] == position_b[i] and position_a[i - 1] != position_b[i - 1]:
         cnt += 1
-    
+
 print(cnt)
