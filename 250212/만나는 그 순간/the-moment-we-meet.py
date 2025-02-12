3fc337c604deeaab31667f960717a916379a057c
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-12 12:13:54 +0900

    Title: 만나는 그 순간, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250212/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,35 @@
+N, M = map(int, input().split())
+
+pos_a, pos_b = [0] * 1001, [0] * 1001 
+time_a, time_b = 1, 1
+
+for _ in range(N):
+    d, t = input().split()
+    if d == 'R':
+        for _ in range(int(t)):
+            pos_a[time_a] = pos_a[time_a - 1] + 1
+            time_a += 1
+    else:
+        for _ in range(int(t)):
+            pos_a[time_a] = pos_a[time_a - 1] - 1
+            time_a += 1
+
+for _ in range(M):
+    d, t = input().split()
+    if d == 'R':
+        for _ in range(int(t)):
+            pos_b[time_b] = pos_b[time_b - 1] + 1
+            time_b += 1
+    else:
+        for _ in range(int(t)):
+            pos_b[time_b] = pos_b[time_b - 1] - 1
+            time_b += 1
+    
+
+ans = -1
+for i in range(1, time_a):
+    if pos_a[i] == pos_b[i]:
+        ans = i
+        break
+
+print(ans)
