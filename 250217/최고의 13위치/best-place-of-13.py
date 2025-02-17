7f667e4c5d57aaa272476c6194b899151c7787c8
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-17 11:55:22 +0900

    Title: 최고의 13위치, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250217/최고의 13위치/best-place-of-13.py
+++ 250217/최고의 13위치/best-place-of-13.py
@@ -1,19 +1,16 @@
-N = int(input())
+# 변수 선언 및 입력
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-coins = []
-for _ in range(N):
-    coins.append(list(map(int, input().split())))
-
+# 모든 쌍을 다 잡아봅니다.
 max_cnt = 0
-for i in range(N):
-    for j in range(N-2):
-        cnt = 0
-        if coins[i][j] == 1:
-            cnt += 1
-        if coins[i][j+1] == 1:
-            cnt += 1
-        if coins[i][j+2] == 1:
-            cnt += 1
-    max_cnt = max(max_cnt, cnt)
-
+for i in range(n):
+    # 격자를 벗어나지 않을 범위로만 잡습니다.
+    for j in range(n - 2):
+        cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+        max_cnt = max(max_cnt, cnt)
+    
 print(max_cnt)
