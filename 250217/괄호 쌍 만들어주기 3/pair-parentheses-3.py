8c7fe9d27471f545c6dbf04139ff577acab1ebf3
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-17 11:26:05 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250217/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,10 @@
+A = input()
+
+answer = 0
+for i in range(len(A)):
+    if A[i] == '(':
+        for j in range(i, len(A)):
+            if A[j] == ')':
+                answer += 1
+
+print(answer)
