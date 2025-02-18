5612d73a722d6765240aa3b1a9b0841965953181
Daehyun <112569789+DaehyunYoo@users.noreply.github.com>
2025-02-18 10:12:42 +0900

    Title: G or H 3, Time: 128ms, Memory: 17MB, Status: Passed - CodeTree

--- 250218/G or H 3/G-or-H-3.py
+++ 250218/G or H 3/G-or-H-3.py
@@ -1,18 +1,25 @@
-N, K = map(int, input().split())
-photos = [0] * 10001
+MAX_NUM = 10000
 
-for _ in range(N):
-    nums, alpha = input().split()
-    if alpha == 'G':
-        photos[int(nums) - 1] = 1
-    else:
-        photos[int(nums) - 1] = 2
+# 변수 선언 및 입력
+n, k = tuple(map(int, input().split()))
+arr = [0] * (MAX_NUM + 1)
 
-answer = 0
-for i in range(len(photos)-K+1):
-    max_sum = 0
-    for j in range(i, i+K+1):
-        max_sum += photos[j]
-    answer = max(answer, max_sum)
+for _ in range(n):
+    x, c = tuple(input().split())
+    x = int(x)
+    
+    arr[x] = 1 if c == 'G' else 2
 
-print(answer)
+
+# 모든 구간의 시작점을 잡아봅니다.
+max_sum = 0
+for i in range(MAX_NUM - k + 1):
+    # 해당 구간 내 원소의 합을 구합니다.
+    sum_interval = 0
+    for j in range(i, i + k + 1):
+        sum_interval += arr[j]
+
+    # 최댓값을 구합니다.
+    max_sum = max(max_sum, sum_interval)
+                        
+print(max_sum)
